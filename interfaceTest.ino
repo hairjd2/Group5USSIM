e9b4a2d895bd9db68417acaa44acf2d9601ddf30
hairjd2 <hairjd2@gmail.com>
2023-04-12 09:49:29 -0400

    set the input pins for just the antenna type

--- interfaceTest.ino
+++ interfaceTest.ino
@@ -1,7 +1,16 @@
-void setup() {
+int Atype1 = 5;
+int Atype2 = 6; // seems to be negated
+int Atype3 = 7;
 
+
+void setup() {
+// TODO: setup the pins we want to watch
+
+    pinMode(Atype1, INPUT);
+    pinMode(Atype2, INPUT);
+    pinMode(Atype3, INPUT);
 }
 
 void loop() {
-    
+// TODO: poll for each pin we're getting inputs of
 }
