7e67cc86f6564b6e8ec8fe26600f7cf4f3e4cf4c
Nick Myers <ndmyers79@gmail.com>
2023-04-24 10:13:40 -0400

    Pain

--- sine_test.ino
+++ sine_test.ino
@@ -6,14 +6,14 @@
 
 Adafruit_MCP4725 dac;
 double theta_R = -10 * PI/180;
-int SCAN_TO = 0;
+int SCAN_TO = 1;
 double theta_BW = 2* PI/180;
 double theta_min = -62* PI/180;
 double theta_max = 62* PI/180;
 double max_SINX;
 double min_SINX;
-int min_OMEGAT = sin(1626.01626 * 461.25 + (2*PI)) * pow(10, 5);
-int max_OMEGAT = sin(1626.01626 * 153.75 + (2*PI)) * pow(10, 5);
+double min_OMEGAT = sin(1626.01626 * 461.25 + (2*PI)) ;
+double max_OMEGAT = sin(1626.01626 * 153.75 + (2*PI)) ;
 
 void setup() {
   Wire.begin();
@@ -37,19 +37,19 @@ void generate_sinx(){
         x_min = PI * ((theta_min + q3/50.0) - theta_R) / (1.15*theta_BW);
         temp_min = (sin(x_min)/x_min);
         temp_max = (sin(x_max)/x_max);
-        min_SINX = temp_min * pow(10, 5);
-        max_SINX = temp_max * pow(10, 5);
+        min_SINX = temp_min;
+        max_SINX = temp_max;
     }else{
         x_max = PI * ((theta_max - q1/50.0) - theta_R) / (1.15*theta_BW);
         x_min = PI * ((theta_max - q3/50.0) - theta_R) / (1.15*theta_BW);
         temp_min = (sin(x_min)/x_min);
         temp_max = (sin(x_max)/x_max);
-        min_SINX = temp_min * pow(10, 5);
-        max_SINX = temp_max * pow(10, 5);
+        min_SINX = temp_min;
+        max_SINX = temp_max;
     }
 }
 
-double generate_wave(int t){
+double generate_wave(float t){
     double x;
     double SINX;
     if (SCAN_TO)
@@ -60,6 +60,8 @@ double generate_wave(int t){
         x = PI * ((theta_max - t/50.0) - theta_R) / (1.15*theta_BW);
         SINX = (sin(x)/x);
     }
+
+    if(t == 154) Serial.println(SINX * sin(1626.01626 * t + (2*PI)), DEC);
     return SINX * sin(1626.01626 * t + (2*PI));
 }
 
@@ -67,13 +69,17 @@ void loop() {
   for (int t = 0; t < 616; t++) { // generate a full sine wave cycle
     double sinValue = generate_wave(t); // calculate sine wave value
     sinValue *= pow(10, 5);
-    sinValue += 1;
+    //sinValue += 1;
     int newSinValue = (int)sinValue;
-    Serial.println(min_SINX);
-    Serial.println(max_SINX);
-    //Serial.println(min_OMEGAT);
-    //Serial.println(max_OMEGAT);
-    int dacValue = map(newSinValue, min_SINX * min_OMEGAT, max_SINX * max_OMEGAT, -2047, 2047) / 10; // convert to DAC value
+    if (t == 154){
+      Serial.println(t);
+      Serial.println(sinValue);
+    }
+    // Serial.println(min_SINX);
+    // Serial.println(max_SINX);
+    // Serial.println(min_OMEGAT);
+    // Serial.println(max_OMEGAT);
+    int dacValue = map(sinValue, min_SINX * min_OMEGAT * pow(10,5), max_SINX * max_OMEGAT * pow(10,5), -2047, 2047) / 10; // convert to DAC value
     dacValue = dacValue * 10 + 2047;
     //Serial.println(dacValue);
     dac.setVoltage(dacValue, false); // send DAC value
