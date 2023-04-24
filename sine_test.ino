a974d2739e5efed1bca143951234403adddd2173
Nick Myers <ndmyers79@gmail.com>
2023-04-24 09:34:50 -0400

    Epic

--- sine_test.ino
+++ sine_test.ino
@@ -12,8 +12,8 @@ double theta_min = -62* PI/180;
 double theta_max = 62* PI/180;
 double max_SINX;
 double min_SINX;
-double min_OMEGAT = sin(1626.01626 * 461.25 + (2*PI));
-double max_OMEGAT = sin(1626.01626 * 153.75 + (2*PI));
+int min_OMEGAT = sin(1626.01626 * 461.25 + (2*PI)) * pow(10, 5);
+int max_OMEGAT = sin(1626.01626 * 153.75 + (2*PI)) * pow(10, 5);
 
 void setup() {
   Wire.begin();
@@ -33,45 +33,46 @@ void generate_sinx(){
     double temp_max;
     if (SCAN_TO)
     {
-        x_max = PI * ((theta_min + q1/50) - theta_R) / (1.15*theta_BW);
-        x_min = PI * ((theta_min + q3/50) - theta_R) / (1.15*theta_BW);
-        temp_min = (sin(x_min)/x_min) * pow(10,5);
-        temp_max = (sin(x_max)/x_max) * pow(10,5);
-        min_SINX = temp_min;
-        max_SINX = temp_max;
+        x_max = PI * ((theta_min + q1/50.0) - theta_R) / (1.15*theta_BW);
+        x_min = PI * ((theta_min + q3/50.0) - theta_R) / (1.15*theta_BW);
+        temp_min = (sin(x_min)/x_min);
+        temp_max = (sin(x_max)/x_max);
+        min_SINX = temp_min * pow(10, 5);
+        max_SINX = temp_max * pow(10, 5);
     }else{
-        x_max = PI * ((theta_max - q1/50) - theta_R) / (1.15*theta_BW);
-        x_min = PI * ((theta_max - q3/50) - theta_R) / (1.15*theta_BW);
-        temp_min = (sin(x_min)/x_min) * pow(10,5);
-        temp_max = (sin(x_max)/x_max) * pow(10,5);
-        min_SINX = temp_min;
-        max_SINX = temp_max;
+        x_max = PI * ((theta_max - q1/50.0) - theta_R) / (1.15*theta_BW);
+        x_min = PI * ((theta_max - q3/50.0) - theta_R) / (1.15*theta_BW);
+        temp_min = (sin(x_min)/x_min);
+        temp_max = (sin(x_max)/x_max);
+        min_SINX = temp_min * pow(10, 5);
+        max_SINX = temp_max * pow(10, 5);
     }
 }
 
-int generate_wave(int t){
+double generate_wave(int t){
     double x;
-    int SINX;
+    double SINX;
     if (SCAN_TO)
     {
-        x = PI * ((theta_min + t/50) - theta_R) / (1.15*theta_BW);
-        SINX = (sin(x)/x) * pow(10,5);
+        x = PI * ((theta_min + t/50.0) - theta_R) / (1.15*theta_BW);
+        SINX = (sin(x)/x);
     }else{
-        x = PI * ((theta_max - t/50) - theta_R) / (1.15*theta_BW);
-        SINX = (sin(x)/x) * pow(10,5);
+        x = PI * ((theta_max - t/50.0) - theta_R) / (1.15*theta_BW);
+        SINX = (sin(x)/x);
     }
-    return SINX * sin(1626.01626 * t + (2*PI)) ;
+    return SINX * sin(1626.01626 * t + (2*PI));
 }
 
 void loop() {
   for (int t = 0; t < 616; t++) { // generate a full sine wave cycle
-    float sinValue = generate_wave(t); // calculate sine wave value
+    double sinValue = generate_wave(t); // calculate sine wave value
+    sinValue *= pow(10, 5);
     sinValue += 1;
     int newSinValue = (int)sinValue;
     Serial.println(min_SINX);
     Serial.println(max_SINX);
-    Serial.println(min_OMEGAT);
-    Serial.println(max_OMEGAT);
+    //Serial.println(min_OMEGAT);
+    //Serial.println(max_OMEGAT);
     int dacValue = map(newSinValue, min_SINX * min_OMEGAT, max_SINX * max_OMEGAT, -2047, 2047) / 10; // convert to DAC value
     dacValue = dacValue * 10 + 2047;
     //Serial.println(dacValue);
