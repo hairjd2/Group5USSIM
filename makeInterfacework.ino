f07fcae4d2e47dd9084a2ec923ad21b1507720bf
Nick Myers <ndmyers79@gmail.com>
2023-05-12 10:22:29 -0400

    Additions to test DAC

--- makeInterfacework.ino
+++ makeInterfacework.ino
@@ -1,10 +1,12 @@
 #include <Wire.h>
 #include <Adafruit_MCP4725.h>
 #include "AZ.h"
-#define DAC_ADDRESS 0x62
+#define DAC_ADDRESS 0x60
 
-const int TxEnpin = 10;
-int i;
+const int transDataSCL = 27;
+const int transDataSDA = 26;
+const int TxEnpin = 13;
+int j;
 Adafruit_MCP4725 dac;
 
 void setup() {
@@ -12,16 +14,31 @@ void setup() {
     dac.begin(DAC_ADDRESS);
     dac.setVoltage(0, false);
     pinMode(TxEnpin, INPUT);
-    i = 30;
+    pinMode(transDataSCL, OUTPUT);
+    pinMode(transDataSDA, OUTPUT);
+    j = 30;
     Serial.begin(9600);
     delay(25);
 }
 
 void loop() {
-    Serial.println(i);
 
-    if(loop == HIGH) {
-        dac.setVoltage(data[i], false);
-        i++;
+    if(TxEnpin == HIGH) {
+        for(int i = 0; i < 1000; i++) {
+          int dacValue = int(data[i]);
+
+          dac.setVoltage(dacValue, false);
+          delayMicroseconds(100);
+        }
+
+        for(int i = 999; i >= 0; i--) {
+          int dacValue = int(data[i]);
+
+          dac.setVoltage(dacValue, false);
+          delayMicroseconds(100);
+        }
+        // dac.setVoltage(data[i], false);
+        Serial.println(j);
+        j++;
     }
 }
